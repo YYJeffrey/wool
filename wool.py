684aae50d6b8634135aae791715f9e01f551a3b2
YYJeffrey <501934027@qq.com>
2019-05-12 10:27:22 +0800

    fead:增加请求超时时间

--- wool.py
+++ wool.py
@@ -7,6 +7,7 @@ import re
 import os
 
 TRY_COUNT = 16  # 获取ID尝试次数
+TIME_OUT = 35  # 请求超时时间
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.81"
                   " Safari/537.36"
@@ -25,13 +26,13 @@ class Chaocuo:
 
     def _get_site_cookie(self):
         # 获取站内cookie
-        self.cookies = requests.get(url=self.url, headers=HEADERS).cookies
+        self.cookies = requests.get(url=self.url, headers=HEADERS, timeout=TIME_OUT).cookies
 
     def get_email(self):
         # 获取一个邮箱地址
         self._get_site_cookie()
         data = {"data": "666", "type": "renew", "arg": "d=027168.com_f="}
-        html = requests.post(url=self.url, data=data, cookies=self.cookies, headers=HEADERS)
+        html = requests.post(url=self.url, data=data, cookies=self.cookies, headers=HEADERS, timeout=TIME_OUT)
         self.cookies = html.cookies
         email = eval(html.text)['data'][0] + "@027168.com"
         self.email = email
@@ -42,7 +43,7 @@ class Chaocuo:
         # 获取最新的邮件id
         time.sleep(2)
         data = {"data": self.email.split("@")[0], "type": "refresh", "arg": ""}
-        html = requests.post(url=self.url, data=data, cookies=self.cookies, headers=HEADERS)
+        html = requests.post(url=self.url, data=data, cookies=self.cookies, headers=HEADERS, timeout=TIME_OUT)
         self.count += 1
         # noinspection PyBroadException
         try:
@@ -63,7 +64,7 @@ class Chaocuo:
         self._get_mid()
         arg = "f={mid}".format(mid=self.mid)
         data = {"data": self.email.split("@")[0], "type": "mailinfo", "arg": arg}
-        html = requests.post(url=self.url, data=data, cookies=self.cookies, headers=HEADERS)
+        html = requests.post(url=self.url, data=data, cookies=self.cookies, headers=HEADERS, timeout=TIME_OUT)
         code = re.search(r"<b>(.*?)<\\/b>", html.text.strip()).group(1)
         print("[成功] 获取验证码 Code:{code}".format(code=code))
         self.code = code
@@ -83,7 +84,7 @@ class Wiki:
     def get_code(self):
         # 发送验证码到邮箱
         data = {"email": self.email}
-        html = requests.post(url=self.url + "auth/send", data=data, headers=HEADERS)
+        html = requests.post(url=self.url + "auth/send", data=data, headers=HEADERS, timeout=TIME_OUT)
         print("[提示] " + eval(html.text)['msg'])
 
     def register(self, code):
@@ -96,7 +97,7 @@ class Wiki:
             "code": "",
             "emailcode": code
         }
-        html = requests.post(url=self.url + "auth/register", data=data, headers=HEADERS)
+        html = requests.post(url=self.url + "auth/register", data=data, headers=HEADERS, timeout=TIME_OUT)
         print("[提示] " + eval(html.text)['msg'])
 
     def login(self):
@@ -105,12 +106,12 @@ class Wiki:
             "email": self.email,
             "passwd": self.email
         }
-        html = requests.post(url=self.url + "auth/login", data=data, headers=HEADERS)
+        html = requests.post(url=self.url + "auth/login", data=data, headers=HEADERS, timeout=TIME_OUT)
         self.cookies = html.cookies
         print("[成功] 开始获取节点地址" if eval(html.text)['msg'].strip() == "欢迎回来" else "[失败] 结束获取节点地址")
 
     def _get_node_arg(self):
-        html = requests.get(url=self.url + "user/node", cookies=self.cookies, headers=HEADERS)
+        html = requests.get(url=self.url + "user/node", cookies=self.cookies, headers=HEADERS, timeout=TIME_OUT)
         res = re.findall(r'<a href="javascript:void\(0\);" onClick="urlChange\((.*?)\)', html.text.strip())
         self.node_arg = res
 
@@ -125,7 +126,7 @@ class Wiki:
             url = self.url + "user/node/{0}?ismu={1}&relay_rule={2}".format(item[0][1:-1], item[1], item[2])
             # noinspection PyBroadException
             try:
-                html = requests.get(url=url, cookies=self.cookies, headers=HEADERS)
+                html = requests.get(url=url, cookies=self.cookies, headers=HEADERS, timeout=TIME_OUT)
                 ss = re.search(r"var text_qrcode2 = '(.*?)'", html.text).group(1)
                 print(ss)
                 self.ssr.append(ss)
